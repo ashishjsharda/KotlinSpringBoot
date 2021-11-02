65aece5a09a4b638af453162fd4025a9cfbf875f
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-02 06:03:24 -0400

    Add function pollPlanes

--- src/main/kotlin/com/ashish/kotlindemo/PlaneFinderPoller.kt
+++ src/main/kotlin/com/ashish/kotlindemo/PlaneFinderPoller.kt
@@ -4,6 +4,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToFlux
 
 @Component
 @EnableScheduling
@@ -11,6 +12,11 @@ class PlaneFinderPoller(private val repository: AircraftRepository) {
     private val client= WebClient.create("http://localhost:7634/aircraft")
     @Scheduled(fixedRate=1000)
     private fun pollPlanes(){
+        repository.deleteAll()
+        client.get().retrieve().bodyToFlux<Aircraft.Aircraft>().filter{!it.reg.isNullOrEmpty()}
+            .toStream().forEach {repository.save(it)}
+        println("All Aircraft")
+        repository.findAll().forEach { println(it)}
 
     }
 }
