a9c2ca617d5a9bb58c18d1234a0aae370e31dd9a
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-02 05:53:38 -0400

    Add stub pollPlanes

--- src/main/kotlin/com/ashish/kotlindemo/PlaneFinderPoller.kt
+++ src/main/kotlin/com/ashish/kotlindemo/PlaneFinderPoller.kt
@@ -1,9 +1,16 @@
 package com.ashish.kotlindemo
 
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
 
 @Component
 @EnableScheduling
 class PlaneFinderPoller(private val repository: AircraftRepository) {
+    private val client= WebClient.create("http://localhost:7634/aircraft")
+    @Scheduled(fixedRate=1000)
+    private fun pollPlanes(){
+
+    }
 }
